765607cab5abc673c58c1644f3f6efac459367c3
mayur <mayur.gupta@tothenew.com>
2018-02-22 21:15:18 +0530

    adding in build library process provided by node js - The process object is a global that provides information about, and control over, the current Node.js process. As a global, it is always available to Node.js applications without using require().
    - to get the user inputs added yargs dependency that helps to fetch easily the input data

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 console.log('asdas');
+
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-var user = os.userInfo();
-fs.appendFile('greetings.txt', `Hello ${user.username}!`)
-console.log('Result:', notes.add(9, -2));
+const argv = yargs.argv;
+
+/*var user = os.userInfo();
+fs.appendFile('greetings.txt', `Hello ${user.username}!`)*/
+//console.log('Result:', notes.add(9, -2));
+process.argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+});
+
+
+console.log(process.argv);
+
+
+console.log('argv value', argv);
+
