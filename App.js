4264a6a020adfbfbef72c2aa2148b8b6ef0a7bc6
mayur <mayur.gupta@tothenew.com>
2018-03-08 13:17:27 +0530

    - added code for listing notes

--- App.js
+++ App.js
@@ -16,7 +16,9 @@ if (command === 'add') {
         console.log('Note title taken');
     }
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach(note => notes.logNote(note));
 } else if (command === 'read') {
     const note = notes.getNote(argv.title);
     if (note) {
--- notes.js
+++ notes.js
@@ -26,7 +26,7 @@ const addNote = (title, body) => {
 };
 
 const getAll = () => {
-    console.log('Getting all notes');
+    return fetchNotes();
 };
 
 const getNote = (title) => {
